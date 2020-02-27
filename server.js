3cc9b200829ee808ff918522c1e828e042a7aa27
Mason Halstead <mason@cognitiv.ai>
2020-02-26 16:53:50 -0800

    favicon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { PRIVATE_KEY, NODE_ENV } = process.env;
 
 require('./startup/config')(PRIVATE_KEY);
-// require('./startup/favicon')(app);
+require('./startup/favicon')(app);
 require('./startup/errors')(app);
 // require('./startup/routes')(app);
 // require('./startup/lookups')();
