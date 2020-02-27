13c9ae0520999fe6454023814fb6ad24dbb211ca
Mason Halstead <mason@cognitiv.ai>
2020-02-26 16:52:36 -0800

    1 at atime

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const { PRIVATE_KEY, NODE_ENV } = process.env;
 
-// require('./startup/config')(PRIVATE_KEY);
+require('./startup/config')(PRIVATE_KEY);
 // require('./startup/favicon')(app);
 require('./startup/errors')(app);
 // require('./startup/routes')(app);
